4bed9b3833b8ee3a9d5e8d36dc7b059713356024
PCdMattex125 <matteomerollim1@gmail.com>
2025-12-26 17:17:06 +0100

    Main scheletro + query di prova

---
+++ main.py
@@ -0,0 +1,59 @@
+import os
+from pathlib import Path
+
+import psycopg
+from dotenv import load_dotenv
+
+def main():
+    load_dotenv() #get virtual env 
+    dsn = os.getenv("DATABASE_URL")
+    if not dsn:
+        print("RIP database")
+        return
+
+    with psycopg.connect(dsn) as conn: 
+        # 1) Crea tabelle
+        run_sql(conn, "ddl.sql")
+
+        # 2) Popola tabelle
+        run_sql(conn, "dati.sql")
+
+        # 3) querys
+        run_queries(conn)
+    print("Completato con successo.")
+
+#built path per gestire DB
+BASE_DIR = Path(__file__).parent
+SQL_DIR = BASE_DIR / "sql"
+
+def read_sql(filename: str) -> str:
+    return (SQL_DIR / filename).read_text(encoding="utf-8")
+
+def run_sql(conn: psycopg.Connection, filename: str) -> None:
+    sql = read_sql(filename) #get filename path
+    with conn.cursor() as cur:
+        cur.execute(sql) #exe query (MUST BE DDL OR DML Plssss!)
+
+def run_queries(conn: psycopg.Connection) -> None:
+    #session
+    with conn.cursor() as cur:
+        Nquery = 1
+        #query lanciata
+        cur.execute(""" 
+            select * from AZIENDA;
+        """)
+        print("query number:", Nquery)
+        Nquery += 1
+        result = cur.fetchall()#sql response
+        if result: 
+            i=0
+            for row in result:
+                print(i, "-->", row)
+                i += 1
+        else:
+            print("Nessun risultato trovato")
+        
+        
+
+if __name__ == "__main__":
+    main()
--- sql/dati.sql
+++ sql/dati.sql
@@ -1,1 +1,4 @@
 -- INSERT INTO ...
+--AZIENDA (PartitaIVA, RagioneSociale, Telefono, Email, StatoCollaborazione)
+INSERT INTO AZIENDA
+VALUES ('12345678901', 'MichealsCorp', '+39 0123 456789', 'info@michealscorp.com', 'Attivo');
