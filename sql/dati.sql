cb2e9915bce7df00f68ddff68b9ed432fe238686
Olga3678 <brzlgo05@gmail.com>
2026-01-08 12:24:28 +0100

    Update dati.sql

--- sql/dati.sql
+++ sql/dati.sql
@@ -1,4 +1,14 @@
 -- INSERT INTO ...
 --AZIENDA (PartitaIVA, RagioneSociale, Telefono, Email, StatoCollaborazione)
-INSERT INTO AZIENDA
-VALUES ('12345678901', 'MichealsCorp', '+39 0123 456789', 'info@michealscorp.com', 'Attivo');
+INSERT INTO AZIENDA (PartitaIVA, RagioneSociale, Telefono, Email, StatoCollaborazione)
+VALUES 
+('11223344556', 'Orthopedica Italiana S.r.l.', '061234567', 'ordini@orthoita.it', 'Attivo'),
+('22334455667', 'Mobility & Care S.p.A.', '029988776', 'info@mobilitycare.com', 'Attivo'),
+('33445566778', 'TechMed Solutions S.n.c.', '011554433', 'commerciale@techmed.it', 'In Valutazione'),
+('44556677889', 'Sollievo S.r.l. Unipersonale', '081223344', 'supporto@sollievo.org', 'Attivo'),
+('55667788990', 'Nuova Ottica & Audio S.r.l.', '051445566', 'amministrazione@noa.it', 'Sospeso'),
+('66778899001', 'BioMeccanica Europea S.p.A.', '049887766', 'export@biomec.eu', 'Attivo'),
+('77889900112', 'Ausili Digitali S.r.l.', '010332211', 'info@ausilidigitali.it', 'In Valutazione'),
+('88990011223', 'Riabilita Shop S.a.s.', '070665544', 'vendite@riabilitashop.it', 'Attivo'),
+('99001122334', 'Global Health Devices S.r.l.', '035221100', 'quality@globalhealth.com', 'Attivo'),
+('00112233445', 'EcoSanity S.p.A.', '045990011', 'contatti@ecosanity.it', 'Chiuso');
